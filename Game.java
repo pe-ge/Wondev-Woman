5e12a4855902d9d37628525a28193bbb8c8b13d3
pe-ge <gergelp@gmail.com>
2017-06-28 19:06:33 +0200

    legalActions implemented

--- Game.java
+++ Game.java
@@ -24,6 +24,7 @@ class Unit {
     char level;
 
     static HashMap<String, Point> actionToPos; // action -> (x, y)
+    static HashMap<String, String[]> pushToDirections;
 
     static {
         actionToPos = new HashMap<String, Point>();
@@ -35,6 +36,16 @@ class Unit {
         actionToPos.put("SW",   new Point(-1, 1));
         actionToPos.put("S",    new Point(0, 1));
         actionToPos.put("SE",   new Point(1, 1));
+
+        pushToDirections = new HashMap<String, String[]>();
+        pushToDirections.put("NW",  new String[]{"N", "NW", "W"});
+        pushToDirections.put("N",  new String[]{"N", "NW", "NE"});
+        pushToDirections.put("NE",  new String[]{"N", "NE", "E"});
+        pushToDirections.put("W",  new String[]{"NW", "SW", "W"});
+        pushToDirections.put("E",  new String[]{"NE", "SE", "E"});
+        pushToDirections.put("SW",  new String[]{"W", "SW", "S"});
+        pushToDirections.put("S",  new String[]{"SW", "S", "SE"});
+        pushToDirections.put("SE",  new String[]{"S", "SE", "E"});
     }
 
     public Unit(int x, int y) {
@@ -51,26 +62,31 @@ class Unit {
         this.level = other.level;
     }
 
+    public String toString() {
+        return position.toString();
+    }
+
     public void moveAndBuild(Action action, char[][] map) {
         move(action.move);
         build(action.build, map);
     }
 
     public void pushAndBuild(Action action, Unit[] pushedUnits, char[][] map) {
-        push(action.move, pushedUnits);
-        build(action.build, map);
+        push(action.move, action.build, pushedUnits);
+        build(action.move, map);
     }
 
-    public void push(String push, Unit[] pushedUnits) {
-        Point direction = actionToPos.get(push);
-        int x = position.x + direction.x;
-        int y = position.y + direction.y;
+    public void push(String push, String where, Unit[] pushedUnits) {
+        Point pushDirection = actionToPos.get(push);
+        int x = position.x + pushDirection.x;
+        int y = position.y + pushDirection.y;
 
         for (Unit unit : pushedUnits) {
             if (unit.position.x == x && unit.position.y == y) {
                 // we found unit to be pushed
-                unit.position.x += direction.x;
-                unit.position.y += direction.y;
+                Point whereDirection = actionToPos.get(where);
+                unit.position.x += whereDirection.x;
+                unit.position.y += whereDirection.y;
                 return;
             }
         }
@@ -123,6 +139,22 @@ class State {
 
     boolean myUnitTurn;
 
+    int myScore, enemyScore;
+
+    static ArrayList<String> directions;
+
+    static {
+        directions = new ArrayList<String>();
+        directions.add("NW");
+        directions.add("N");
+        directions.add("NE");
+        directions.add("W");
+        directions.add("E");
+        directions.add("SW");
+        directions.add("S");
+        directions.add("SE");
+    }
+
     public State(boolean myUnitTurn) {
         map = new char[size][size];
         this.myUnitTurn = myUnitTurn;
@@ -155,15 +187,25 @@ class State {
     }
 
     public void printMap() {
+        System.err.print(" ");
         for (int i = 0; i < map.length; i++) {
+            System.err.print(i);
+        }
+        System.err.println();
+        for (int i = 0; i < map.length; i++) {
+            System.err.print(i);
             for (int j = 0; j < map[i].length; j++) {
-                System.err.print(String.format("%02d ", map[i][j]));
+                System.err.print(map[i][j]);
             }
             System.err.println();
         }
+        for (int i = 0; i < unitsPerGame; i++) {
+            System.err.println("My unit: " + myUnits[i]);
+            System.err.println("Enemy unit: " + enemyUnits[i]);
+        }
     }
 
-    public State copyState() {
+    public State copyState(boolean switchTurn) {
         State copied = new State(this.myUnitTurn);
         copied.map = new char[size][size];
         for (int i = 0; i < size; i++) {
@@ -177,8 +219,12 @@ class State {
         for (int i = 0; i < unitsPerGame; i++) {
             copied.myUnits[i] = new Unit(myUnits[i]);
             copied.enemyUnits[i] = new Unit(enemyUnits[i]);
-
         }
+        copied.myUnitTurn = switchTurn ? !myUnitTurn : myUnitTurn;
+
+        copied.myScore = myScore;
+        copied.enemyScore = enemyScore;
+
         return copied;
     }
 
@@ -187,6 +233,13 @@ class State {
         switch (action.atype) {
             case "MOVE&BUILD":
                 executingUnit.moveAndBuild(action, map);
+                if (map[executingUnit.position.y][executingUnit.position.x] == '3') {
+                    if (myUnitTurn) {
+                        myScore++;
+                    } else {
+                        enemyScore++;
+                    }
+                }
                 break;
             case "PUSH&BUILD":
                 Unit[] otherUnits = myUnitTurn ? enemyUnits : myUnits;
@@ -195,6 +248,143 @@ class State {
         }
         return this;
     }
+
+    public Point canMove(String move, Point position, Unit[] units, Unit[] otherUnits, char[][] map) {
+        Point moveTo = canBuild(move, position, units, otherUnits, map);
+        if (moveTo == null) {
+            return null;
+        }
+
+        char currentLevel = map[position.y][position.x];
+        char moveToLevel = map[moveTo.y][moveTo.x];
+        if (currentLevel + 1 < moveToLevel) {
+            return null;
+        }
+
+        return moveTo;
+    }
+
+    public Point canBuild(String move, Point position, Unit[] units, Unit[] otherUnits, char[][] map) {
+        Point direction = Unit.actionToPos.get(move);
+        int x = position.x + direction.x;
+        int y = position.y + direction.y;
+
+        if (!legalCell(x, y, map)) {
+            return null;
+        }
+
+        for (Unit unit : units) {
+            if (unit.position.x == x && unit.position.y == y) {
+                return null;
+            }
+        }
+        for (Unit unit : otherUnits) {
+            if (unit.position.x == x && unit.position.y == y) {
+                return null;
+            }
+        }
+        return new Point(x, y);
+    }
+
+    public boolean legalCell(int x, int y, char[][] map) {
+        if (x < 0 || y < 0 || x >= State.size || y >= State.size) {
+            return false;
+        }
+        if (map[y][x] == '.' || map[y][x] == '4') {
+            return false;
+        }
+
+        return true;
+    }
+
+    public Unit getNeighbourUnit(Unit myUnit, String direction, Unit[] units, char[][] map) {
+        Point directionPoint = Unit.actionToPos.get(direction);
+        int x = myUnit.position.x + directionPoint.x;
+        int y = myUnit.position.y + directionPoint.y;
+
+        if (!legalCell(x, y, map)) {
+            return null;
+        }
+
+        for (Unit unit : units) {
+            if (unit.position.x == x && unit.position.y == y) {
+                return unit;
+            }
+        }
+
+        return null;
+    }
+
+    // public Unit canPush(String push, Point position, Unit[] units, Unit[] otherUnits, char[][] map) {
+        // Point direction = Unit.actionToPos.get(push);
+        // int x = position.x + direction.x;
+        // int y = position.y + direction.y;
+
+        // if (!legalCell(x, y, map)) {
+            // return null;
+        // }
+
+        // for (Unit unit : otherUnits) {
+            // if (unit.position.x == x && unit.position.y == y) {
+                // int pushedX = x + direction.x;
+                // int pushedY = y + direction.y;
+                // if (legalCell(pushedX, pushedY, map)) {
+                    // return unit;
+                // }
+                // return null;
+            // }
+        // }
+        // return null;
+    // }
+
+    public ArrayList<Action> legalActions() {
+        ArrayList<Action> legalActions = new ArrayList<Action>();
+        Unit[] units = myUnitTurn ? myUnits : enemyUnits;
+        Unit[] otherUnits = myUnitTurn ? enemyUnits : myUnits;
+
+        for (int i = 0; i < unitsPerGame; i++) {
+            Unit unit = units[i];
+            // MOVE&BUILD
+            // first move
+            for (String moveDirection : directions) {
+                Point movePoint = canMove(moveDirection, unit.position, units, otherUnits, map);
+                if (movePoint != null) {
+                    // then build
+                    int tmpX = unit.position.x;
+                    int tmpY = unit.position.y;
+                    unit.position.x = -1;
+                    unit.position.y = -1;
+                    for (String buildDirection : directions) {
+                        Point buildPoint = canBuild(buildDirection, movePoint, units, otherUnits, map);
+                        if (buildPoint != null) {
+                            Action action = new Action("MOVE&BUILD", i, moveDirection, buildDirection);
+                            legalActions.add(action);
+                        }
+                    }
+                    unit.position.x = tmpX;
+                    unit.position.y = tmpY;
+                }
+            }
+
+            // PUSH&BUILD
+            // push
+            for (String pushDirection : directions) {
+                Unit pushedUnit = getNeighbourUnit(unit, pushDirection, otherUnits, map);
+                if (pushedUnit != null) {
+                    // System.err.println("mozem tiskat " + pushDirection);
+                    for (String moveDirection : Unit.pushToDirections.get(pushDirection)) {
+                        // check whether pushedTo position is free
+                        Point pushedTo = canMove(moveDirection, pushedUnit.position, units, otherUnits, map);
+                        if (pushedTo != null) {
+                            Action action = new Action("PUSH&BUILD", i, pushDirection, moveDirection);
+                            legalActions.add(action);
+                        }
+                    }
+                }
+            }
+        }
+        return legalActions;
+    }
 }
 
 class Game {
@@ -202,8 +392,6 @@ class Game {
     Scanner input;
 
     State state;
-
-    ArrayList<String> directions;
 
     Random random;
 
@@ -214,16 +402,6 @@ class Game {
         State.unitsPerGame = input.nextInt();
 
         state = new State(true);
-
-        directions = new ArrayList<String>();
-        directions.add("NW");
-        directions.add("N");
-        directions.add("NE");
-        directions.add("W");
-        directions.add("E");
-        directions.add("SW");
-        directions.add("S");
-        directions.add("SE");
 
         random = new Random();
     }
@@ -239,67 +417,79 @@ class Game {
         return state.map[y][x] != -2 && state.map[y][x] != 4 && state.map[y][x] <= 10;
     }
 
-    public Action findMove() {
-        Action bestAction = new Action("MOVE&BUILD", 0, null, null);
-        for (String direction : directions) {
-            // System.err.println(action);
-            Point nextPosition = next(state.myUnits[0].position, direction);
+    // public Action findMove() {
+        // Action bestAction = new Action("MOVE&BUILD", 0, null, null);
+        // for (String direction : directions) {
+            // // System.err.println(action);
+            // Point nextPosition = next(state.myUnits[0].position, direction);
 
-            if (!checkPosition(nextPosition)) {
-                continue;
-            }
-            int nextPositionLevel = state.map[nextPosition.y][nextPosition.x];
-            if (nextPositionLevel == state.myUnits[0].level + 1) {
-                bestAction.move = direction;
-                bestAction.level = nextPositionLevel;
-                bestAction.position = nextPosition;
-                // System.err.println(nextPosition);
-                return bestAction;
-            }
-            if (nextPositionLevel >= bestAction.level && nextPositionLevel <= state.myUnits[0].level + 1) {
-                bestAction.move = direction;
-                bestAction.level = nextPositionLevel;
-                bestAction.position = nextPosition;
-            }
+            // if (!checkPosition(nextPosition)) {
+                // continue;
+            // }
+            // int nextPositionLevel = state.map[nextPosition.y][nextPosition.x];
+            // if (nextPositionLevel == state.myUnits[0].level + 1) {
+                // bestAction.move = direction;
+                // bestAction.level = nextPositionLevel;
+                // bestAction.position = nextPosition;
+                // // System.err.println(nextPosition);
+                // return bestAction;
+            // }
+            // if (nextPositionLevel >= bestAction.level && nextPositionLevel <= state.myUnits[0].level + 1) {
+                // bestAction.move = direction;
+                // bestAction.level = nextPositionLevel;
+                // bestAction.position = nextPosition;
+            // }
 
-        }
+        // }
 
-        if (bestAction.move == null) {
-            System.err.println("No possible action found");
-            // int randomIdx = random.nextInt(directions.size());
-            // bestAction.move = directions.get(randomIdx);
-            // bestAction.position = next(state.myUnits[0].position, bestAction.move);
-            // bestAction.level = state.map[bestAction.position.y][bestAction.position.x];
-        }
+        // if (bestAction.move == null) {
+            // System.err.println("No possible action found");
+        // }
 
-        return bestAction;
-    }
+        // return bestAction;
+    // }
 
-    public Action findBuild(Action action) {
-        Point nextPosition = action.position;
-        int maxLevel = 0;
-        for (String direction : directions) {
-            Point buildPosition = next(nextPosition, direction);
-            if (!checkPosition(buildPosition)) {
-                continue;
-            }
-            int buildPositionLevel = state.map[buildPosition.y][buildPosition.x];
-            if (buildPositionLevel >= maxLevel && buildPositionLevel <= action.level) {
-                maxLevel = buildPositionLevel;
-                action.build = direction;
-            }
-        }
-        return action;
-    }
+    // public Action findBuild(Action action) {
+        // Point nextPosition = action.position;
+        // int maxLevel = 0;
+        // for (String direction : directions) {
+            // Point buildPosition = next(nextPosition, direction);
+            // if (!checkPosition(buildPosition)) {
+                // continue;
+            // }
+            // int buildPositionLevel = state.map[buildPosition.y][buildPosition.x];
+            // if (buildPositionLevel >= maxLevel && buildPositionLevel <= action.level) {
+                // maxLevel = buildPositionLevel;
+                // action.build = direction;
+            // }
+        // }
+        // return action;
+    // }
 
     public void mainLoop() {
+        state.readState(input);
+        state.printMap();
         while (true) {
-            state.readState(input);
+            long time = System.currentTimeMillis();
+            System.err.println(state.legalActions());
+            System.err.println("passed time for legal actions :" + (System.currentTimeMillis() - time));
+            Action a = new Action(
+                input.next(), // atype
+                input.nextInt(), // index
+                input.next(), // move
+                input.next()
+            ); // build
+            state.executeAction(a);
             state.printMap();
-            Action action = findMove();
-            action = findBuild(action);
-            System.out.println(action);
+            System.err.println("SCORE: " + state.myScore);
         }
+        // while (true) {
+            // state.readState(input);
+            // state.printMap();
+            // Action action = findMove();
+            // action = findBuild(action);
+            // System.out.println(action);
+        // }
     }
 
     public static void main(String args[]) {
